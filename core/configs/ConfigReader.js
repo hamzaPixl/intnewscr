238c857868b154c23a80de40ec018a8f30a1b6a7
mounir hamza <imha@emakina.com>
2017-03-06 11:44:35 +0100

    [INTNEWSCR-19] created ConfigReader

---
+++ core/configs/ConfigReader.js
@@ -0,0 +1,147 @@
+function ConfigReader(moduleName, moduleLocator, configRoutePath, valueRoutePath) {
+    this.moduleName = moduleName;
+    this.moduleLocator = moduleLocator;
+    this.configRoutePath = configRoutePath || './../../modules/';
+    this.valueRoutePath = valueRoutePath || './components/';
+
+}
+
+ConfigReader.prototype = {
+
+    /**
+     * returns the configuration value
+     * 
+     * @see ModuleLocator::configIsValid()
+     * 
+     * 
+     * @private
+     * 
+     * @return null if definition and value doesn't match
+     * @return {{*}} if configuration is valid
+     */
+    getConfig: function getConfig() {
+        if (!this.configIsValid()) {
+            return null;
+        }
+        return this.getModuleConfigurationValue();
+    },
+
+    /**
+     * returns the true if de definition match to value 
+     * 
+     * @see ModuleLocator::getModuleConfigurationDefinition()
+     * @see ModuleLocator::getModuleConfigurationValue()
+     * 
+     * 
+     * @private
+     * 
+     * @return true if definition and value doesn't match
+     * @return false if it is valid
+     */
+    configIsValid: function configIsValid() {
+
+        let configCheck = this.getModuleConfigurationDefinition();
+        let configValue = this.getModuleConfigurationValue();
+
+        if (!configValue && !configCheck) {
+            return false;
+        }
+
+        configCheck = configCheck['config'];
+
+        for (var key in configCheck) {
+            var ob = configCheck[key];
+            if (ob.require) {
+                if (configValue[ob.name] == undefined || typeof(configValue[ob.name]) !== ob.type) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    },
+
+    /**
+     * returns the modules services  
+     * 
+     * @see ConfigReader::getModuleConfigurationDefinition()
+     * 
+     * @return null if module doesn't exists
+     * @return {{*}} if module exists  
+     */
+    getServices: function getServices() {
+        let config = this.getModuleConfigurationDefinition();
+        if (!config) {
+            return null;
+        }
+        return config['services'];
+    },
+
+    /**
+     * returns the modulename 
+     * 
+     * 
+     * @return null if module doesn't exists
+     * @return {{*}} if it exists  
+     */
+    getModuleName: function getModuleName() {
+        return this.moduleName;
+    },
+
+    /**
+     * returns the moduleLocator 
+     * 
+     * 
+     * @return null if module doesn't exists
+     * @return {{*}} if it exists  
+     */
+    getModuleLocator: function getModuleLocator() {
+        return this.moduleLocator;
+    },
+
+    /**
+     * returns the modules configuration value 
+     * 
+     * test if the module module exists:
+     * we test that by using the moduleLocator.
+     * 
+     * 
+     * @see ModuleLocator::moduleExists()
+     * 
+     * @private
+     * 
+     * @return null if module doesn't exists
+     * @return {{*}} if module exists  
+     */
+    getModuleConfigurationValue: function getModuleConfigurationValue() {
+        if (!this.moduleLocator.moduleExists(this.moduleName)) {
+            return null;
+        }
+        let path = this.valueRoutePath + this.moduleLocator.getModule(this.moduleName).value;
+        return require(path);
+    },
+    /**
+     * returns the modules configuration definition 
+     * 
+     * test if the module module exists:
+     * we test that by using the moduleLocator.
+     * 
+     * @see ModuleLocator::moduleExists()
+     * 
+     * @private
+     * 
+     * @return null if module doesn't exists
+     * @return {{*}} if module exists  
+     */
+    getModuleConfigurationDefinition: function getModuleConfigurationDefinition() {
+        if (!this.moduleLocator.moduleExists(this.moduleName)) {
+            return null;
+        }
+        let path = this.configRoutePath + this.moduleLocator.getModule(this.moduleName).check;
+        return require(path);
+    },
+
+
+};
+
+module.exports = ConfigReader;
