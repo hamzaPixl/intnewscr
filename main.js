d117ebd45bca35fb6a3aac5e8974bdd3a018ff15
mounir hamza <imha@emakina.com>
2017-03-08 15:15:08 +0100

    [INTNEWSCR-22] created the main to launch backend app

---
+++ main.js
@@ -0,0 +1,22 @@
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+const Controller = require('./core/controllers/Controller');
+const controller = new Controller();
+
+
+io.on('connection', function(client) {
+
+    client.on('request', function(request) {
+        controller.request(request).then(function(data) {
+            client.emit('response', data);
+        }, function(err) {
+            console.log(err);
+        });
+    });
+
+    client.on('disconnect', function() {});
+
+});
+
+server.listen(3000);
+console.log('Server started');
