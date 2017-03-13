9416959b6cbc687f61b3a8fe7047ca3e7c79000e
mounir hamza <imha@emakina.com>
2017-03-13 10:18:22 +0100

    [INTNEWSCR-3] Add comments

--- modules/news/Controller.js
+++ modules/news/Controller.js
@@ -15,6 +15,14 @@ function Controller(moduleLocator, config, services) {
 
 Controller.prototype = {
 
+    /**
+     * Call the buisness if there are no data in database
+     * else it return the data that the databse contains
+     * 
+     * It gives news only if parameters are correctly sets
+     * 
+     * @return Promis that contains data
+     */
     getNews: function getNews(params) {
 
         if (!params || !params.source) {
@@ -38,6 +46,15 @@ Controller.prototype = {
 
     },
 
+    /**
+     * Call the buisness if there are no data in database
+     * else it return the data that the databse contains
+     * 
+     * It gives news only if parameters are correctly sets
+     * It gives news from an API
+     * 
+     * @return Promis that contains data
+     */
     getNewsApi: function getNews(params) {
 
         if (!params || !params.source) {
@@ -61,6 +78,15 @@ Controller.prototype = {
 
     },
 
+    /**
+     * Call the buisness if there are no data in database
+     * else it return the data that the databse contains
+     * 
+     * It gives news only if parameters are correctly sets
+     * It gives trafic informations
+     * 
+     * @return Promis that contains data
+     */
     getNewsTrafic: function getNewsTrafic() {
         var trafic = this.repositoryTrafic.findAllBy('model', TraficItem.getName());
         if (trafic && trafic.length > 0) {
