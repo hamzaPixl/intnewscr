b1604011be75b362e05b10bbe389965935146f2b
mounir hamza <imha@emakina.com>
2017-03-14 11:53:55 +0100

    [INTNEWSCR] Add eslint for weather module

--- modules/weather/Buisness.js
+++ modules/weather/Buisness.js
@@ -1,30 +1,30 @@
 function Buisness(services, config) {
-    this.services = services;
-    this.config = config;
-    for (var key in this.services) {
-        this[key] = require(this.services[key].name);
-    }
+  this.services = services;
+  this.config = config;
+  Object.keys(this.services).map((serviceKey) => {
+    this[serviceKey] = require(this.services[serviceKey].name);
+    return this;
+  });
 }
 
 Buisness.prototype = {
 
     /**
      * It search the weather forecast for Brussels on Belgium
-     * and degrees are un C 
+     * and degrees are un C
      * @return Promise with data
      */
-    getWeather: function getWeather() {
-        return new Promise(function(resolve, reject) {
-            this.weatherJs.find({ search: 'Brussels, Belgium', degreeType: 'C' }, function(err, result) {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(result[0].forecast);
-                }
-            });
-        }.bind(this));
-    },
-
+  getWeather: function getWeather() {
+    return new Promise((resolve, reject) => {
+      this.weatherJs.find({ search: 'Brussels, Belgium', degreeType: 'C' }, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result[0].forecast);
+        }
+      });
+    });
+  },
 };
 
 module.exports = Buisness;
--- modules/weather/Main.js
+++ modules/weather/Main.js
@@ -1,25 +1,25 @@
 function Main() {
-    this.routes = {
-        '/weather': {
-            method: 'GET',
-            callback: 'getWeather',
-        }
-    };
+  this.routes = {
+    '/weather': {
+      method: 'GET',
+      callback: 'getWeather',
+    },
+  };
 }
 
 Main.prototype = {
 
     /**
-     * Get a route from the url 
+     * Get a route from the url
      * null if it is not defined
      * @param url is the url needed
      */
-    getRoute: function getRoute(url) {
-        if (!this.routes[url])
-            return null;
-        return this.routes[url];
+  getRoute: function getRoute(url) {
+    if (!this.routes[url]) {
+      return null;
     }
-
+    return this.routes[url];
+  },
 };
 
 module.exports = Main;
