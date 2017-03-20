ece532c650e6cd74f90e5675f1d3ddb8cadd60b6
mounir hamza <imha@emakina.com>
2017-03-20 15:48:07 +0100

    [INTNEWSCR-5] Main module created

---
+++ modules/clients/Main.js
@@ -0,0 +1,28 @@
+function Main() {
+  this.routes = {
+    '/facebook': {
+      method: 'GET',
+      callback: 'getFacebookPosts',
+      query: {
+        source: 'string',
+      },
+    },
+  };
+}
+
+Main.prototype = {
+
+    /**
+     * Get a route from the url
+     * null if it is not defined
+     * @param url is the url needed
+     */
+  getRoute: function getRoute(url) {
+    if (!this.routes[url]) {
+      return null;
+    }
+    return this.routes[url];
+  },
+};
+
+module.exports = Main;
