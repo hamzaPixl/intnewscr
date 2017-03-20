ada3c2f2fdc80f4a2024fbd08eea66b8b0820f39
mounir hamza <imha@emakina.com>
2017-03-20 16:55:43 +0100

    [INTNEWSCR-5] Add function in buisness

--- modules/clients/Buisness.js
+++ modules/clients/Buisness.js
@@ -9,7 +9,25 @@ function Buisness(services, config) {
 
 Buisness.prototype = {
 
-    /**
+     /**
+     * It search the 2 latest news of the source on google
+     * @param source is the query search
+     * @return Promise with data
+     */
+  getGooglePosts: function getGooglePosts(source) {
+    return new Promise((resolve, reject) => {
+      const url = this.config.url.replace('source', source);
+      this.rss.parseURL(url, (err, parsed) => {
+        if (err) {
+          reject(err);
+        } else {
+          const result = parsed.feed.entries.map((item) => { const temp = item; temp.source = source; return temp; });
+          resolve(result.slice(0, 2));
+        }
+      });
+    });
+  },
+     /**
      * It search the 2 latest post of the source on facebook
      * @param sourceid is the page id of the client
      * @return Promise with data
