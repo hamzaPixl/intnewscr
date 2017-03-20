564e93265e8dad225e701bbf5ae15e71de46fbc3
mounir hamza <imha@emakina.com>
2017-03-20 16:54:48 +0100

    [INTNEWSCR-5] Add callback function for new route

--- modules/clients/Controller.js
+++ modules/clients/Controller.js
@@ -1,13 +1,14 @@
 const Buisness = require('./Buisness');
 const Repository = require('./../../core/models/Repository');
 const FacebookItem = require('./models/FacebookItem');
-
+const GoogleItem = require('./models/GoogleItem');
 
 function Controller(config, services) {
   this.config = config;
   this.services = services;
   this.buisness = new Buisness(this.services, this.config);
-  this.repository = new Repository(FacebookItem);
+  this.repoFacebook = new Repository(FacebookItem);
+  this.repoGoogle = new Repository(GoogleItem);
 }
 
 Controller.prototype = {
@@ -18,7 +19,7 @@ Controller.prototype = {
      * @return Promis that contains data
      */
   getFacebookPosts: function getFacebookPosts(params) {
-    const posts = this.repository.findAllBy('source', this.config[params.source]);
+    const posts = this.repoFacebook.findAllBy('source', this.config[params.source]);
     if (posts && posts.length > 0) {
       return new Promise((resolve, reject) => {
         if (!posts) {
@@ -32,8 +33,34 @@ Controller.prototype = {
         if (!postsBuis) {
           reject(postsBuis);
         }
-        this.repository.saveAll(postsBuis);
-        resolve(this.repository.findAllBy('source', this.config[params.source]));
+        this.repoFacebook.saveAll(postsBuis);
+        resolve(this.repoFacebook.findAllBy('source', this.config[params.source]));
+      });
+    });
+  },
+
+  /**
+     * Call the buisness if there are no data in database
+     * else it return the data that the databse contains
+     * @return Promis that contains data
+     */
+  getGooglePosts: function getGooglePosts(params) {
+    const posts = this.repoGoogle.findAllBy('source', params.source);
+    if (posts && posts.length > 0) {
+      return new Promise((resolve, reject) => {
+        if (!posts) {
+          reject(posts);
+        }
+        resolve(posts);
+      });
+    }
+    return new Promise((resolve, reject) => {
+      this.buisness.getGooglePosts(params.source).then((postsBuis) => {
+        if (!postsBuis) {
+          reject(postsBuis);
+        }
+        this.repoGoogle.saveAll(postsBuis);
+        resolve(this.repoGoogle.findAllBy('source', params.source));
       });
     });
   },
