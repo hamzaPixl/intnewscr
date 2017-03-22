c905df0940103e37e694a0b33ea6f3d6ac056643
mounir hamza <imha@emakina.com>
2017-03-22 11:06:12 +0100

    [INTNEWSCR-29] Main admin module created

---
+++ core/admin/Main.js
@@ -0,0 +1,32 @@
+function Main() {
+  this.routes = {
+    '/getAll': {
+      method: 'GET',
+      callback: 'getAll',
+    },
+    '/getRoutes': {
+      method: 'GET',
+      callback: 'getRoutes',
+      query: {
+        module: 'string',
+      },
+    },
+  };
+}
+
+Main.prototype = {
+
+  /**
+  * Get a route from the url
+  * null if it is not defined
+  * @param url is the url needed
+  */
+  getRoute: function getRoute(url) {
+    if (!this.routes[url]) {
+      return null;
+    }
+    return this.routes[url];
+  },
+};
+
+module.exports = Main;
