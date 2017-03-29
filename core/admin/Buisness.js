57aaf180428546409328a9e5f625ba8ecac6d78f
mounir hamza <imha@emakina.com>
2017-03-29 15:51:58 +0200

    [INTNEWSCR-34] Remove routes

--- core/admin/Buisness.js
+++ core/admin/Buisness.js
@@ -12,37 +12,17 @@ function Buisness (services, config) {
 Buisness.prototype = {
 
   /**
-   *
-   * @returns {Promise}
+   * It gives the default parameter for all routes
+   * @private
+   * @return Array
    */
-  testAll: function testAll () {
-    const result = this.db.get('parameters').map().value();
-    /* return new Promise((resolve, reject) => {
-     const result = [];
-     this.db.get({}, (err, docs) => {
-     if (err || !docs) {
-     return;
-     }
-     Object.keys(docs).map((route) => {
-     const request = new this.RequestModel(`${route}`, `${docs[route].module}${route}`, docs[route].params);
-     const controller = new this.Controller();
-     controller.request(request).then((data) => {
-     if (!data) {
-     result[route] = false;
-     } else {
-     result[route] = true;
-     }
-     }).catch((err) => {result[route] = true;});
-     return this;
-     });
-     resolve(result);
-     });
-     });*/
+  getDefaultParameter: function getDefaultParameter () {
+    return this.db.get('parameters').value();
   },
 
   /**
    * Get all modules that are
-   * available in the app
+   * available in the app, test them to give their status and their default parameter
    * @see ModuleLocator
    * @return Promise
    */
@@ -52,24 +32,11 @@ Buisness.prototype = {
       if (!modules) {
         reject('Nothing found');
       } else {
-        resolve(modules);
+        resolve({modules, default: this.getDefaultParameter()});
       }
     });
   },
-  /**
-   * It test a route by calling the module
-   * @param params contains param to call the module service
-   * @returns {Promise}
-   */
-  testRoute: function testRoute (params) {
-    return new Promise((resolve, reject) => {
-      const request = new this.RequestModel(`${params.url}`, `${params.route}`, params.params);
-      const controller = new this.Controller();
-      controller.request(request).then((data) => {
-        resolve(data);
-      });
-    });
-  },
+
   /**
    * Get all modules routes that are
    * available in the app
