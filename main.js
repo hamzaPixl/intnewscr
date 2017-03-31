1827572bcb623471b1afe8c15ed7ff534fa047dd
mounir hamza <imha@emakina.com>
2017-03-31 10:41:36 +0200

    [INTNEWSCR-35] Add credentials for outlook

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
+const url = require('url');
 require('dotenv').config();
-const server = require('http').createServer();
-const io = require('socket.io')(server);
 const Controller = require('./core/controllers/Controller');
+
+/**
+ * This function handle all request that the server receive
+ * @param request
+ * @param response
+ */
+function onRequest (request, response) {
+  const pathname = url.parse(request.url).pathname;
+  const query = url.parse(request.url).query;
+  switch (pathname) {
+    default:
+      break;
+  }
+}
+
+const server = require('http').createServer(onRequest);
+const io = require('socket.io')(server);
 
 const controller = new Controller();
 
 io.on('connection', (client) => {
   client.on('request', (requestClient) => {
-    const result = controller.request(requestClient,client);
+    const result = controller.request(requestClient, client);
     if (result) {
       result.then((data) => {
         client.emit('response', {data, name: requestClient.name});
-      }).catch((err) => {
+      }).catch(() => {
         client.emit('response', {error: 'Error from the widget !', name: requestClient.name});
       });
     } else {
@@ -22,4 +38,5 @@ io.on('connection', (client) => {
 });
 
 server.listen(process.env.PORT || 3000);
+
 console.log('Server started');
