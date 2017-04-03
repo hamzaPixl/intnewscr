7bbd08ad92adb97f72520c5adcabdd74c90c9566
mounir hamza <imha@emakina.com>
2017-04-03 16:15:31 +0200

    [INTNEWSCR-35] add the refresh token

--- modules/auth/outlook/models/TokenItem.js
+++ modules/auth/outlook/models/TokenItem.js
@@ -38,6 +38,7 @@ TokenItem.prototype = {
       model: this.model,
       token: this.token,
       expires_in: this.expires_in,
+      refreshToken: this.refreshToken,
     });
   },
 
@@ -52,6 +53,7 @@ TokenItem.prototype = {
     this.model = data.model;
     this.token = data.token;
     this.expires_in = data.expires_in;
+    this.refreshToken = data.refreshToken;
   },
 
   /**
@@ -62,9 +64,10 @@ TokenItem.prototype = {
     this.created = Date.now();
     this.model = TokenItem.getName();
     this.ttl = TokenItem.getTTL();
-    this.id = this.created;
-    this.expires_in = json.expires_in;
-    this.token = json.access_token;
+    this.id = json.outlookID;
+    this.expires_in = 0;
+    this.token = json.accessToken;
+    this.refreshToken = json.refreshToken;
   },
 
 };
