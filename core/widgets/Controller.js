b8882019fc3aba82eeea2eb7fb03c2f50f2592ca
mounir hamza <imha@emakina.com>
2017-04-11 12:25:31 +0200

    [INTNEWSR-15] add function for widgets crud

--- core/widgets/Controller.js
+++ core/widgets/Controller.js
@@ -14,9 +14,36 @@ Controller.prototype = {
     return this.db.get('widgets').value();
   },
 
+  /**
+   * Create a widget
+   * @param params
+   * @return {*}
+   */
   createWidget: function createWidget (params) {
-    const widget = new WidgetItem(Date.now(), params.ttl, params.name, params.request);
-    this.db.push(widget)
+    const widget = new WidgetItem(params.name + Date.now(), params.ttl, params.name, params.request);
+    this.db.push(widget).write();
+    return this.getAll();
+  },
+
+  /**
+   * Update a widget
+   * @param params
+   * @return {*}
+   */
+  updateWidget: function updateWidget (params) {
+    this.db.find({id: params.id})
+      .assign({request: params.request})
+      .write();
+    return this.getAll();
+  },
+
+  /**
+   * Delete a widgets
+   * @param params
+   * @return {*}
+   */
+  deleteWidget: function deleteWidget (params) {
+    this.db.remove({id: params.id})
       .write();
     return this.getAll();
   },
