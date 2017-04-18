e3ee60b39f4321235c287352ca0de6331cc79b55
mounir hamza <imha@emakina.com>
2017-04-18 13:44:30 +0200

    [INTNEWSCR-14] CHANGE PATH FILE DB

--- modules/views/Controller.js
+++ modules/views/Controller.js
@@ -1,8 +1,8 @@
 const low = require('lowdb');
 
 function Controller () {
-  this.widgets_db = low('./../database/widgets.json', {storage: require('lowdb/lib/storages/file-async')});
-  this.views_db = low('./../database/views.json', {storage: require('lowdb/lib/storages/file-async')});
+  this.widgets_db = low('./database/widgets.json', {storage: require('lowdb/lib/storages/file-async')});
+  this.views_db = low('./database/views.json', {storage: require('lowdb/lib/storages/file-async')});
 }
 
 Controller.prototype = {
@@ -12,7 +12,7 @@ Controller.prototype = {
    */
   getAll: function getAll () {
     return new Promise((resolve, reject) => {
-      resolve({views:this.widgets_db.get('views').value(),widgets:this.widgets_db.get('widgets').value()});
+      resolve({views: this.views_db.get('views').value(), widgets: this.widgets_db.get('widgets').value()});
     });
   },
 
