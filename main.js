492be7b1404151fa876c10f6d7e703bf363d653c
Hamza Mounir <hamza.pixelle@gmail.com>
2017-10-21 17:32:25 +0200

    REMOVE UNUSED CODE

--- main.js
+++ main.js
@@ -5,24 +5,3 @@ const server = new Server();
 
 server.start();
 
-/*
-io.on('connection', (client) => {
-  client.on('request', (requestClient) => {
-    if (requestClient.name === 'viewChange') {
-      io.sockets.emit('response', {request: {name: 'viewChange', view: requestClient.view}});
-    } else {
-      const result = controller.request(requestClient, client);
-      if (result) {
-        result.then((data) => {
-          client.emit('response', {data, name: requestClient.name, request: requestClient});
-        }).catch((err) => {
-          client.emit('response', {error: 'Error from the widget !', name: requestClient.name, request: requestClient});
-        });
-      } else {
-        client.emit('response', {error: 'Error from the request !', name: requestClient.name, request: requestClient});
-      }
-    }
-  });
-  client.on('disconnect', () => {});
-});
-*/
