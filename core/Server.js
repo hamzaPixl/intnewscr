97a39c101096a7989fd27701b652adb755eb6b45
Hamza Mounir <hamza.pixelle@gmail.com>
2017-10-22 16:37:58 +0200

    USE JSEND MIDDELWARES

--- core/Server.js
+++ core/Server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const jsend = require('jsend');
 const logger = require('../tools/logger');
 
 const modules = require('./modules');
@@ -17,13 +18,15 @@ class Server {
    * @memberof Server
    */
   setMidelwares() {
-    this.app.use((req, res, next) => { logger.log(`${req.ip} - ${req.method} - ${req.baseUrl} - ${req.path}`); });
+    this.app.use((req, res, next) => { logger.log(`${req.ip} - ${req.method} - ${req.originalUrl} - ${req.baseUrl} - ${req.path}`); next(); });
 
     this.app.use(bodyParser.urlencoded({ extended: false }));
 
     this.app.use(cookieParser());
 
     this.app.use(bodyParser.json());
+
+    this.app.use(jsend.middleware);
 
     this.app.use('/', modules);
   }
@@ -33,7 +36,7 @@ class Server {
    * @memberof Server
    */
   start() {
-    logger.log(`Start the server on ${process.env.APP_URL}${process.env.PORT} ...`);
+    logger.log(`The server is started on URL : ${process.env.APP_URL}${process.env.PORT}`);
     this.app.listen(process.env.PORT || 3000);
   }
 
