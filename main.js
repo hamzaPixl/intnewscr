8b2a3803fab9ef196035b21f2be322fa48885311
Hamza Mounir <hamza.pixelle@gmail.com>
2017-10-29 00:30:52 +0200

    config file before connect to database

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
+require('./config').searchENV();
 const Server = require('./core/Server');
 const Database = require('./database');
-const config = require('./config');
 
 const server = new Server();
 const database = new Database();
-
-config.searchENV();
 
 database.connect().then(() => {
   server.start();
