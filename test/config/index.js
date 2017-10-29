6e6c06a02eca6b5f41516ec964b6ad93eb4b3e55
Hamza Mounir <hamza.pixelle@gmail.com>
2017-10-29 19:29:02 +0100

    test configure

--- test/config/index.js
+++ test/config/index.js
@@ -1,19 +1,9 @@
+require('../../config').searchENV();
 const assert = require('assert');
 
 describe('Configuration variables', () => {
   describe('Try to connect to the database', () => {
-    it('should not have the process env custom', () => {
-      assert.equal(process.env.ENV, undefined);
-      assert.equal(process.env.JWT_SECRET, undefined);
-      assert.equal(process.env.PORT, undefined);
-      assert.equal(process.env.APP_URL, undefined);
-      assert.equal(process.env.DB_PORT, undefined);
-      assert.equal(process.env.DB_NAME, undefined);
-      assert.equal(process.env.DB_URL, undefined);
-    });
-
     it('should return the process env', () => {
-      require('../../config').searchENV();
       assert.ok(process.env.ENV);
       assert.ok(process.env.JWT_SECRET);
       assert.ok(process.env.USER);
