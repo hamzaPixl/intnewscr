66482e9cab99be9eee80f9de7cc47ef4587efc33
Hamza Mounir <hamza.pixelle@gmail.com>
2017-10-29 12:34:25 +0100

    npm run clean ended

--- tools/clean-db.js
+++ tools/clean-db.js
@@ -1,10 +1,13 @@
 require('../config').searchENV();
 const database = require('../database');
 const collections = require('../database/models/collections.json');
+const logger = require('./logger');
 
 database.connect()
 .then((db) => {
   Object.keys(collections).map((key) => {
-    return db.collection(key).drop();
+    logger.log(`This collection will be empty : ${key}`);
+    return db.collection(key).remove({});
   });
+  logger.log('The database is successfuly cleared');
 }).catch();
