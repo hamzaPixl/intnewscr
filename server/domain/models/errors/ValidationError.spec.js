b125f844c9a30a499596c148d14059ab435f691d
Hamza Mounir <hamza.pixelle@gmail.com>
2018-10-20 22:26:30 +0200

    add domaine error test

---
+++ server/domain/models/errors/ValidationError.spec.js
@@ -0,0 +1,21 @@
+const ValidationError = require('./ValidationError');
+
+describe('INT NEWS SCREEN - models - errors - ValidationError', () => {
+  it('should create a valid instance', () => {
+    const error = new ValidationError();
+    expect(error).toBeDefined();
+    expect(error.message).toBe('Validation Error');
+    expect(error.name).toBe('ValidationError');
+  });
+  it('should create a valid instance with specific message', () => {
+    const error = new ValidationError('Specific message');
+    expect(error.message).toBe('Specific message');
+    expect(error.name).toBe('ValidationError');
+  });
+  it('should create a valid instance with specific details', () => {
+    const error = new ValidationError('Specific message', {}, { a: 4 });
+    expect(error.message).toBe('Specific message');
+    expect(error.name).toBe('ValidationError');
+    expect(error.details).toMatchObject({ a: 4 });
+  });
+});
