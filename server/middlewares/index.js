3409a649134cdd59cb849778b71981db30283191
Hamza Mounir <hamza.pixelle@gmail.com>
2018-11-19 11:33:51 +0100

    tools

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,11 +1,9 @@
 const errorHandlerMiddleware = require('./errorHandlerMiddleware');
 const errorLoggerMiddleware = require('./errorLoggerMiddleware');
 const notFoundMiddleware = require('./notFoundMiddleware');
-const authenticationMiddleware = require('./authenticationMiddleware');
 
 module.exports = {
   errorHandlerMiddleware,
   errorLoggerMiddleware,
   notFoundMiddleware,
-  authenticationMiddleware,
 };
