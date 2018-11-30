1b1ec0784a4235ed40101102b8dad67c62562b0b
Hamza Mounir <hamza.pixelle@gmail.com>
2018-11-30 13:08:59 +0100

    change to json file

--- husky.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  husky: {
-    hooks: {
-      'pre-commit': 'hooks/check-test-only.sh',
-      'prepare-commit-msg': 'hooks/prepare-commit-msg.sh ${GIT_PARAMS}',
-      'pre-push': 'hooks/check-no-console-call.sh && hooks/run-test-prepush.sh',
-    },
-  },
-};
