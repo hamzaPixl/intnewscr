2327b77e951b084d7bf7ebd8698898ff8a1077c1
Hamza Mounir <hamza.pixelle@gmail.com>
2018-12-03 10:33:52 +0100

    [INTNEWSCR] add factory for configurations model

--- server/widgets/admin/domain/factories/index.js
+++ server/widgets/admin/domain/factories/index.js
@@ -1,7 +1,9 @@
 const userFactory = require('./userFactory');
 const viewFactory = require('./viewFactory');
+const configurationFactory = require('./configurationFactory');
 
 module.exports = {
   viewFactory,
   userFactory,
+  configurationFactory,
 };
