43d15764c3ecf80fbd37e761a6d31d7cfa8deb14
Hamza Mounir <hamza.pixelle@gmail.com>
2018-12-25 16:14:31 +0100

    [INTNEWSCR] services

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,14 +1,27 @@
-import { authHeader } from '../_helpers';
+import authHeader from '../helpers/auth-header';
 
-export const userService = {
-  login,
-  logout,
-  register,
-  getAll,
-  getById,
-  update,
-  delete: _delete,
-};
+function logout() {
+  // remove user from local storage to log user out
+  localStorage.removeItem('user');
+}
+
+function handleResponse(response) {
+  return response.text().then((text) => {
+    const data = text && JSON.parse(text);
+    if (!response.ok) {
+      if (response.status === 401) {
+        // auto logout if 401 response returned from api
+        logout();
+      }
+
+      const error = (data && data.message) || response.statusText;
+      return Promise.reject(error);
+    }
+
+    return data;
+  });
+}
+
 
 function login(username, password) {
   const requestOptions = {
@@ -20,9 +33,7 @@ function login(username, password) {
   return fetch('/users/authenticate', requestOptions)
     .then(handleResponse)
     .then((user) => {
-      // login successful if there's a jwt token in the response
       if (user.token) {
-        // store user details and jwt token in local storage to keep user logged in between page refreshes
         localStorage.setItem('user', JSON.stringify(user));
       }
 
@@ -30,10 +41,6 @@ function login(username, password) {
     });
 }
 
-function logout() {
-  // remove user from local storage to log user out
-  localStorage.removeItem('user');
-}
 
 function getAll() {
   const requestOptions = {
@@ -73,30 +80,11 @@ function update(user) {
   return fetch(`/users/${user.id}`, requestOptions).then(handleResponse);
 }
 
-// prefixed function name with underscore because delete is a reserved word in javascript
-function _delete(id) {
-  const requestOptions = {
-    method: 'DELETE',
-    headers: authHeader(),
-  };
-
-  return fetch(`/users/${id}`, requestOptions).then(handleResponse);
-}
-
-function handleResponse(response) {
-  return response.text().then((text) => {
-    const data = text && JSON.parse(text);
-    if (!response.ok) {
-      if (response.status === 401) {
-        // auto logout if 401 response returned from api
-        logout();
-        location.reload(true);
-      }
-
-      const error = (data && data.message) || response.statusText;
-      return Promise.reject(error);
-    }
-
-    return data;
-  });
-}
+export {
+  login,
+  logout,
+  register,
+  getAll,
+  getById,
+  update,
+};
