274a3dcd275ca330eecb0e4bfff493526d8f01bd
Hamza Mounir <hamza.pixelle@gmail.com>
2018-12-25 16:12:36 +0100

    [INTNEWSCR] add base app

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+
+import store from './helpers/store';
 import App from './containers/App';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
 
