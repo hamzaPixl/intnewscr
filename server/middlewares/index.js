73c6f34db3bc5e8ac626817a579b39c6bf2e21b2
Hamza Mounir <hamza.pixelle@gmail.com>
2019-01-01 16:05:13 +0100

    [INTNEWSCR] add to index cors

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -2,10 +2,12 @@ const errorHandlerMiddleware = require('./errorHandlerMiddleware');
 const errorLoggerMiddleware = require('./errorLoggerMiddleware');
 const notFoundMiddleware = require('./notFoundMiddleware');
 const docMiddleware = require('./docMiddleware');
+const corsMiddleware = require('./corsMiddleware');
 
 module.exports = {
   errorHandlerMiddleware,
   errorLoggerMiddleware,
   docMiddleware,
   notFoundMiddleware,
+  corsMiddleware,
 };
